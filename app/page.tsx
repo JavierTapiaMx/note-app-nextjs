12a0243062baf8ae3f10f835a1ba8083fac975de
Javier Tapia <mj.tapia@hotmail.com>
2025-11-18 00:08:58 -0600

    Fix spacing in heading element on HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div className="flex flex-row items-center justify-center p-8">
-      <h1 className="text-3xl font-bold ">📝 Note Taking App</h1>
+      <h1 className="text-3xl font-bold">📝 Note Taking App</h1>
     </div>
   );
 };
