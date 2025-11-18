1cad334b8b25ac648e873a5caa6b1c8f477c0eb0
Javier Tapia <mj.tapia@hotmail.com>
2025-11-18 14:55:33 -0600

    Add React Query Devtools to enhance debugging capabilities in the ReactQueryProvider

--- providers/reactQueryProvider.tsx
+++ providers/reactQueryProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode, useState } from "react";
 
 export function ReactQueryProvider({ children }: { children: ReactNode }) {
@@ -16,5 +17,10 @@ export function ReactQueryProvider({ children }: { children: ReactNode }) {
       })
   );
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
